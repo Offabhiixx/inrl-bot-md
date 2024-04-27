e74893c6232359c5e96f8485a2733c85b75d5494
Offabhiixx <167756222+Offabhiixx@users.noreply.github.com>
2024-04-27 16:56:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 process.env.NODE_OPTIONS = '--max_old_space_size=2560'//2.5
 const DB_URL =  process.env.DATABASE_URL || '';
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', //your ssid to run bot
+    SESSION_ID: process.env.SESSION_ID || 'inrl~a2531MouTfeb54ec2c0056adb042dce1b224', //your ssid to run bot
     HEROKU: {
         API_KEY: process.env.HEROKU_API_KEY,
         APP_NAME: process.env.HEROKU_APP_NAME
@@ -20,9 +20,9 @@ module.exports = {
     PM_BLOCK : toBool(process.env.PM_BLOCK || "false"),
     BGMBOT : toBool(process.env.BGMBOT || "false"),
     CALL_BLOCK : toBool(process.env.CALL_BLOCK || "false"),
-    STATUS_VIEW : process.env.STATUS_VIEW || "false",
+    STATUS_VIEW : process.env.STATUS_VIEW || "true",
     SAVE_STATUS : toBool(process.env.SAVE_STATUS || "false"),
-    ADMIN_SUDO_ACCESS: toBool(process.env.ADMIN_SUDO_ACCESS || "false"),
+    ADMIN_SUDO_ACCESS: toBool(process.env.ADMIN_SUDO_ACCESS || "true"),
     DISABLE_PM: toBool(process.env.DISABLE_PM || "false"),
     DISABLE_GRP : toBool(process.env.DISABLE_GRP || "false"),
     ERROR_MSG : toBool(process.env.ERROR_MSG || "true"),
@@ -31,17 +31,29 @@ module.exports = {
     CHATBOT : process.env.CHATBOT || "false",//true, pm, group
     REACT : process.env.REACT || "false",//true, command, emoji
     WARNCOUND : process.env.WARNCOUND || 5,
-    BOT_INFO : process.env.BOT_INFO || "INRL-BOT-MD;INRL;https://i.imgur.com/DyLAuEh.jpg",
-    WORKTYPE : process.env.WORKTYPE || "private",
+    BOT_INFO : process.env.BOT_INFO || "ABHIIXXN-BOT-MD;ABHIIXXN;https://i.imgur.com/DyLAuEh.jpg",
+    WORKTYPE : process.env.WORKTYPE || "public",
     PREFIX : process.env.PREFIX || "[.,!]",//both  .  and [.] equal, for multi prefix we use [] this
     LANG : process.env.LANG || "en",
     PERSONAL_MESSAGE: process.env.PERSONAL_MESSAGE || "null",
     BOT_PRESENCE : process.env.BOT_PRESENCE || "unavailable",
-    AUDIO_DATA : process.env.AUDIO_DATA || "INRL-BOT-MD;INRL;https://i.imgur.com/DyLAuEh.jpg",
-    STICKER_DATA : process.env.STICKER_DATA || "inrl;inrl",
+    AUDIO_DATA : process.env.AUDIO_DATA || "ABHIIXXN-BOT-MD;
+ 𝗡𝗼𝘄 𝗽𝗹𝗮𝘆𝗶𝗻𝗴:
+"(Song-Music Artist)" 
+01:23 ━━━━●───── 03:43
+      ㅤ ㅤ◁ㅤ ❚❚ ㅤ▷ ㅤㅤ
+   ---˖⁺. ༶ ⋆˙⊹❀♡❀˖⁺. ༶ ⋆˙⊹---
+;https://i.imgur.com/DyLAuEh.jpg",
+    STICKER_DATA : process.env.STICKER_DATA || "ABHIIXXN🍓;
+ 𝗡𝗼𝘄 𝗽𝗹𝗮𝘆𝗶𝗻𝗴:
+"(Song-Music Artist)" 
+01:23 ━━━━●───── 03:43
+      ㅤ ㅤ◁ㅤ ❚❚ ㅤ▷ ㅤㅤ
+   ---˖⁺. ༶ ⋆˙⊹❀♡❀˖⁺. ༶ ⋆˙⊹---
+",
     BRAINSHOP: process.env.BRAINSHOP || '172372,nbjE0YAlyw3cpoMl',
-    SUDO : process.env.SUDO || "null",
-    RMBG_KEY: process.env.RMBG_KEY,
+    SUDO : process.env.SUDO || "+918089228037",
+    RMBG_KEY: process.env.RMBG_KEY || 'VEKz4WCifN62S7v3Wb7dC5dc',
     OPEN_AI: process.env.OPEN_AI,
     ELEVENLABS: process.env.ELEVENLABS,
     INRL_KEY: process.env.INRL_KEY || 'free50_inrl',
